cec7c1b4cf91b81bf917cf166209b36403c1d671
sillymoose <davidnmfarrell@gmail.com>
2014-05-22 01:00:25 -0400

    Added Example.pm files

---
+++ robot-name/Example.pm
@@ -0,0 +1,19 @@
+class Robot {
+    has $.name is rw = self.reset_name;
+
+    method rand_letter {
+        ['A'..'Z'].pick;
+    }
+
+    method rand_suffix {
+        100 + 900.rand.truncate;
+    }
+
+    method new_name {
+        self.rand_letter ~ self.rand_letter ~ self.rand_suffix;
+    }
+
+    method reset_name {
+        self.name = self.new_name;
+    }
+}
