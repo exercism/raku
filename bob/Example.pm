42cbc45434c1f9b22efea73a9699f97fe8905085
Austin Lyons <austinlyons@users.noreply.github.com>
2014-09-24 07:22:15 -0500

    Update Example.pm
    Woah --> Whoa

--- bob/Example.pm
+++ bob/Example.pm
@@ -1,7 +1,7 @@
 class Bob {
     method hey ($input) {
             if    $input ~~ /^\s*$/                             { 'Fine. Be that way!' }
-            elsif $input ~~ /<:Upper>/ and $input.uc eq $input  { 'Woah, chill out!'   }
+            elsif $input ~~ /<:Upper>/ and $input.uc eq $input  { 'Whoa, chill out!'   }
             elsif $input ~~ /'?'$/                              { 'Sure.'              }
             else                                                { 'Whatever.'          }
         }
